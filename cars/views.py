6a77dfed06a867b9de37133291d98df1cf9b0e40
T Jackson <sweetladyhazle@gmail.com>
2023-01-25 15:51:06 -0600

    Updated Dajango Admin Center + Seralizing cont'd

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import Carserializer
+from .models import Car
 
 @api_view(['GET'])
 def cars_list(request):
     
+   cars = Car.objects.all() 
+   
+   serializer = Carserializer(cars, many=True)
 
-    return Response('ok')
+
+   return Response(serializer.data)
